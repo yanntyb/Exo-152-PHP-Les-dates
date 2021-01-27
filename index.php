c794734a1a6d2a77bbe36006a4dfffc38715f2bd
yann tyb <yanntyb.lbc@gmail.com>
2021-01-27 13:05:50 +0100

    a

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
 
 // Premiere ligne
+$date = date("j/m/Y");
+echo $date;
+echo "<br>";
 
+$d = mktime(0, 0, 0, 12, 10, 2018);
+echo date("Y.m.d", $d);
+echo "<br>";
 
+$d = mktime(11,35,7);
+echo date("g:i:s", $d);
 
 
 
